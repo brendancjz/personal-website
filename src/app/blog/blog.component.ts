117e3feae455b602d7fa8adb0a1a58ff0c5e8525
brendancjz <brendancjz@gmail.com>
2021-12-12 19:50:42 +0800

    new updates

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IBlogParagraph {
+  heading: string | null,
+  text: string | null,
+  image: string | null
+}
+
+export interface IBlog {
+  title: string,
+  date: string,
+  noOfMinutesToRead: string,
+  tldr: string | null,
+  paragraphs: IBlogParagraph[],
+  finalWord: IBlogParagraph | null
+}
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -10,6 +25,21 @@ export class BlogComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createTeleBot: IBlog = {
+    title: "How To: Create a Java Telegram Bot",
+    date: "25 July 2021",
+    noOfMinutesToRead: "8 minute read",
+    tldr: "A guide in developing a Java Telegram Bot",
+    paragraphs: [
+      {
+        heading: null,
+        text: null,
+        image: null
+      }
+    ],
+    finalWord: null
   }
 
 }
