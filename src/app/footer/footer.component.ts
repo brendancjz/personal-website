466f314a0a33683e0fe96b8ec26771a3e5fcc7aa
brendancjz <brendancjz@gmail.com>
2022-08-02 00:48:06 +0800

    Add scroll up button and Update resume

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  hasPageBeenScrolledDown = window.scrollY >= 500;
+  isScrollEventListenerAdded = false;
+
+  constructor() {}
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowResize() {
+
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    const rootElement = document.documentElement;
+
+    if (this.hasPageBeenScrolledDown && !this.isScrollEventListenerAdded) {
+      function scrollToTop() {
+        rootElement.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
+
+      scrollToTopBtn!.addEventListener('click', scrollToTop);
+      this.isScrollEventListenerAdded = true;
+    } else if (!this.hasPageBeenScrolledDown) {
+      this.isScrollEventListenerAdded = false;
+    }
+
+    this.hasPageBeenScrolledDown = window.scrollY >= 500;
+  }
 }
