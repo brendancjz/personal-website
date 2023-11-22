6f57fbcddb7217e889c652c422ae6e331ae97d05
brendancjz <brendancjz@gmail.com>
2023-11-22 22:09:40 +0800

    Update projects to add Virtual Nursing Dashboard

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -2,151 +2,183 @@ import { Component, OnInit } from '@angular/core';
 
 //MyInfo Titles
 export enum MyInfoTitles {
-  ABOUT_ME, EDUCATION, WORK_EXPERIENCE, SKILLS
+  ABOUT_ME,
+  EDUCATION,
+  WORK_EXPERIENCE,
+  SKILLS,
 }
 
 //This is for each content within an MyInfo. example - Accredify under Experience.
 export interface IMyInfoContent {
-  header: string | null,
-  date: string | null,
-  oneliner: string | null,
-  content: string[]
+  header: string | null;
+  date: string | null;
+  oneliner: string | null;
+  content: string[];
 }
 
 //There will be multiple content within one MyInfo
 export interface IMyInfo {
-  title: string,
-  content: IMyInfoContent[]
+  title: string;
+  content: IMyInfoContent[];
 }
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit {
   currentContent: MyInfoTitles = MyInfoTitles.ABOUT_ME;
   myInfoTitles = MyInfoTitles;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   aboutMe: IMyInfo = {
-    title: "About Me",
-    content: [{
-      header: null,
-      date: null,
-      oneliner: null,
-      content: ["Hello! I’m a senior Information Systems major studying in " +
-        "National University of Singapore. I’m an outgoing and optimistic " +
-        "individual who always sees the glass half full.",
-
-      "Currently, looking for opportunities to work with a company that " +
-      "will bring my expertise up a notch and allow me to make an impact in " +
-      "the industry.",
-
-      "These days my time is spent on learning to be a better software engineer, " +
-      "ideating and developing software projects and catching up with my friends.",
-
-      "Out of work, you’ll find me playing computer games with my friends, working out in the gym " +
-      "or playing badminton."]
-    }]
-  }
-
-  education: IMyInfo = {
-    title: "Education",
-    content: [{
-      header: "National University of Singapore",
-      date: "Aug 2020 - Jun 2024",
-      oneliner: "Bachelor of Computing in Information Systems (Honours)",
-      content: ["CCA: NUS FinTech Society — Blockchain Developer, Residential College 4 Student Badminton Coach"]
-    }, {
-      header: "Temasek Junior College",
-      date: "Jan 2016 - Dec 2017",
-      oneliner: "GCE 'A' Levels - Physics, Chemistry, Math, Economics",
-      content: ["CCA: Temasek Xcel Scholar, Badminton Captain for 'A' Division"]
-    }, {
-      header: "Victoria School",
-      date: "Jan 2012 - Dec 2015",
-      oneliner: "GCE 'O' Levels - Triple Science, Literature",
-      content: ["CCA: Badminton Captain for 'B' and 'C' Division"]
-    }]
-  }
-
-  workExp: IMyInfo = {
-    title: "Work Experience",
+    title: 'About Me',
     content: [
       {
-        header: "Goldman Sachs Singapore",
-        date: "Jun 2023 - Aug 2023",
-        oneliner: "Summer Technology Analyst",
-        content: ["Architected multiple data pipelines utilising Java " +
-        "Spring libraries, such as Spring Data Mongo and " +
-        "Elasticsearch, to migrate and sync over 100,000 " +
-        "sets of data from production database to business " +
-        "user interface daily."]
-      }, {
-      header: "Government Technology Agency Singapore",
-      date: "May 2022 - Jan 2023",
-      oneliner: "Full Stack Mobile Engineer",
-      content: ["Developed a feature on a mobile app with a React " +
-                "Native and Next.js frontend and Node.js backend. This " +
-                "feature allows users to submit improvements to any " +
-                "Singapore government agencies through a single entry point.",
-              "Designed and implemented unit and integration tests " +
-              "for multiple endpoints using Jest testing framework.",
-              "Leveraged on Agile methodologies to fulfill project requirements."]
-    }, {
-      header: "NUS Teaching Assistant",
-      date: "Aug 2021 - May 2022",
-      oneliner: "Taught Students on Object-Orientated and Functional Programming",
-      content: ["Guided a group of 25 students through weekly technical assignments " +
-              "with emphasis on good coding practices to develop medium scale Java programs."]
-    }, {
-      header: "Accredify",
-      date: "May 2021 - Aug 2021",
-      oneliner: "Front-End Software Engineering Intern",
-      content: ["Single handedly developed the User Interface for an email template builder and " +
-                "a course program builder to allow clients to freely customise " +
-                "certificate emails and courses intuitively from scratch.",
-                "Built with HTML, CSS and JavaScript " +
-                "while using Bootstrap and jQuery frameworks."]
-    }]
-  }
+        header: null,
+        date: null,
+        oneliner: null,
+        content: [
+          'Hello! I’m a senior Information Systems major studying in ' +
+            'National University of Singapore. I’m an outgoing and optimistic ' +
+            'individual who always sees the glass half full.',
+
+          'Currently, looking for opportunities to work with a company that ' +
+            'will bring my expertise up a notch and allow me to make an impact in ' +
+            'the industry.',
+
+          'These days my time is spent on learning to be a better software engineer, ' +
+            'ideating and developing software projects and catching up with my friends.',
+
+          'Out of work, you’ll find me playing computer games with my friends, working out in the gym ' +
+            'or playing badminton.',
+        ],
+      },
+    ],
+  };
+
+  education: IMyInfo = {
+    title: 'Education',
+    content: [
+      {
+        header: 'National University of Singapore',
+        date: 'Aug 2020 - Jun 2024',
+        oneliner: 'Bachelor of Computing in Information Systems (Honours)',
+        content: [
+          'CCA: NUS FinTech Society — Blockchain Developer, Residential College 4 Student Badminton Coach',
+        ],
+      },
+      {
+        header: 'Temasek Junior College',
+        date: 'Jan 2016 - Dec 2017',
+        oneliner: "GCE 'A' Levels - Physics, Chemistry, Math, Economics",
+        content: [
+          "CCA: Temasek Xcel Scholar, Badminton Captain for 'A' Division",
+        ],
+      },
+      {
+        header: 'Victoria School',
+        date: 'Jan 2012 - Dec 2015',
+        oneliner: "GCE 'O' Levels - Triple Science, Literature",
+        content: ["CCA: Badminton Captain for 'B' and 'C' Division"],
+      },
+    ],
+  };
+
+  workExp: IMyInfo = {
+    title: 'Work Experience',
+    content: [
+      {
+        header: 'Goldman Sachs Singapore',
+        date: 'Jun 2023 - Aug 2023',
+        oneliner: 'Summer Technology Analyst',
+        content: [
+          'Architected multiple data pipelines utilising Java ' +
+            'Spring libraries, such as Spring Data Mongo and ' +
+            'Elasticsearch, to migrate and sync over 100,000 ' +
+            'sets of data from production database to business ' +
+            'user interface daily.',
+        ],
+      },
+      {
+        header: 'Government Technology Agency Singapore',
+        date: 'May 2022 - Jan 2023',
+        oneliner: 'Full Stack Mobile Engineer',
+        content: [
+          'Developed a feature on a mobile app with a React ' +
+            'Native and Next.js frontend and Node.js backend. This ' +
+            'feature allows users to submit improvements to any ' +
+            'Singapore government agencies through a single entry point.',
+          'Designed and implemented unit and integration tests ' +
+            'for multiple endpoints using Jest testing framework.',
+          'Leveraged on Agile methodologies to fulfill project requirements.',
+        ],
+      },
+      {
+        header: 'NUS Teaching Assistant',
+        date: 'Aug 2021 - May 2022',
+        oneliner:
+          'Taught Students on Object-Orientated and Functional Programming',
+        content: [
+          'Guided a group of 25 students through weekly technical assignments ' +
+            'with emphasis on good coding practices to develop medium scale Java programs.',
+        ],
+      },
+      {
+        header: 'Accredify',
+        date: 'May 2021 - Aug 2021',
+        oneliner: 'Front-End Software Engineering Intern',
+        content: [
+          'Single handedly developed the User Interface for an email template builder and ' +
+            'a course program builder to allow clients to freely customise ' +
+            'certificate emails and courses intuitively from scratch.',
+          'Built with HTML, CSS and JavaScript ' +
+            'while using Bootstrap and jQuery frameworks.',
+        ],
+      },
+    ],
+  };
 
   skills: IMyInfo = {
-    title: "Skills & Tech",
-    content: [{
-      header: "10,000+ Lines",
-      date: null,
-      oneliner: null,
-      content: ["Java"]
-    }, {
-      header: "1,000+ Lines",
-      date: null,
-      oneliner: null,
-      content: ["MySQL, HTML, CSS, JavaScript, TypeScript"]
-    }, {
-      header: "I'm familiar with",
-      date: null,
-      oneliner: null,
-      content: ["Angular, Atlassian Suite, Elasticsearch, Git, Jarkata EE, Java Spring Boot, jQuery, MongoDB, PostgreSQL, Postman, Python, React, Swagger.io"]
-    }]
-  }
+    title: 'Skills & Tech',
+    content: [
+      {
+        header: '10,000+ Lines',
+        date: null,
+        oneliner: null,
+        content: ['Java, JavaScript, TypeScript'],
+      },
+      {
+        header: '1,000+ Lines',
+        date: null,
+        oneliner: null,
+        content: ['MySQL, HTML, CSS'],
+      },
+      {
+        header: "I'm familiar with",
+        date: null,
+        oneliner: null,
+        content: [
+          'Angular, Atlassian Suite, Elasticsearch, Git, Jarkata EE, Java Spring Boot, jQuery, MongoDB, PostgreSQL, Postman, Python, React, React Native, Socket.io, Swagger.io',
+        ],
+      },
+    ],
+  };
 
   //Key - Value pair, to correct display which MyInfo Content.
   //NOTE: The result of Enum toString() == 'index of Enum'
-  myInfo: {[key: string]: IMyInfo} = {
+  myInfo: { [key: string]: IMyInfo } = {
     0: this.aboutMe,
     1: this.education,
     2: this.workExp,
-    3: this.skills
-  }
+    3: this.skills,
+  };
 
   updateContentDiv(content: MyInfoTitles) {
     this.currentContent = content;
   }
-
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,120 +2,194 @@ import { Injectable } from '@angular/core';
 import { Project } from '../models/Project';
 
 @Injectable({
-  providedIn : 'root',
+  providedIn: 'root',
 })
 export class ProjectService {
-  constructor() {
-  }
+  constructor() {}
 
   initialiseProjects() {
     const boLui = new Project();
-    boLui.name = "Bo Lui Bot";
-    boLui.date = "May 2021";
-    boLui.oneLiner = "Personal Finance Tracking Bot";
-    boLui.description = "Bo Lui is a quirky Telegram Bot that tracks your " +
-                  "personal spendings and earnings.\n\n It's simplicity, " +
-                  "convenience and fun personality makes finance " +
-                  "tracking more interactive and fun.";
-    boLui.techUsed = ["Heroku CLI", "Java", "Maven", "PostgreSQL", "Telegram API"];
-    boLui.image = "../../assets/images/project-image-bo-lui.jpg";
-    boLui.action = "Try it out";
-    boLui.actionLink = "https://www.t.me/bo_lui_bot/";
-    boLui.githubLink = "https://github.com/brendancjz/boluibot";
-
+    boLui.name = 'Bo Lui Bot';
+    boLui.date = 'May 2021';
+    boLui.oneLiner = 'Personal Finance Tracking Bot';
+    boLui.description =
+      'Bo Lui is a quirky Telegram Bot that tracks your ' +
+      "personal spendings and earnings.\n\n It's simplicity, " +
+      'convenience and fun personality makes finance ' +
+      'tracking more interactive and fun.';
+    boLui.techUsed = [
+      'Heroku CLI',
+      'Java',
+      'Maven',
+      'PostgreSQL',
+      'Telegram API',
+    ];
+    boLui.image = '../../assets/images/project-image-bo-lui.jpg';
+    boLui.action = 'Try it out';
+    boLui.actionLink = 'https://www.t.me/bo_lui_bot/';
+    boLui.githubLink = 'https://github.com/brendancjz/boluibot';
 
     const website = new Project();
-    website.name = "My Website";
-    website.date = "Dec 2021";
-    website.oneLiner = "Personal Website";
-    website.description = "I have always wanted a website with my own domain. " +
-                  "There wasn't a template that I really liked and so, " +
-                  "I decided to develop my own website from scratch.\n\n " +
-                  "This website is intended to showcase my projects, " +
-                  "introduce myself and blog about interesting topics.";
-    website.techUsed = ["Angular", "CSS", "HTML", "TypeScript"];
-    website.image = "";
-    website.action = "";
-    website.actionLink = "";
-    website.githubLink = "https://github.com/brendancjz/personal-website";
+    website.name = 'My Website';
+    website.date = 'Dec 2021';
+    website.oneLiner = 'Personal Website';
+    website.description =
+      'I have always wanted a website with my own domain. ' +
+      "There wasn't a template that I really liked and so, " +
+      'I decided to develop my own website from scratch.\n\n ' +
+      'This website is intended to showcase my projects, ' +
+      'introduce myself and blog about interesting topics.';
+    website.techUsed = ['Angular', 'CSS', 'HTML', 'TypeScript'];
+    website.image = '';
+    website.action = '';
+    website.actionLink = '';
+    website.githubLink = 'https://github.com/brendancjz/personal-website';
 
     const chiaCollection = new Project();
-    chiaCollection.name = "The Chia Collection";
-    chiaCollection.date = "Nov 2018";
-    chiaCollection.oneLiner = "Clothing E-Commerce Platform";
-    chiaCollection.description = "Designed and sold more than 150 articles of clothing worldwide.\n\n" +
-                "I've always enjoyed doodling as a child. Stepping out of my comfort zone, " +
-                "I took up this opportunity to start a business selling t-shirts and " +
-                "hoodies with my own graphic designs.\n\n It's an honour to create something that " +
-                "resonates with someone and this journey has been a wild one.";
-    chiaCollection.techUsed = ["Adobe Illustrator", "Adobe Photoshop", "cPanel", "WooCommerce", "WordPress"];
-    chiaCollection.image = "../../assets/images/project-image-the-chia-collection.jpg";
+    chiaCollection.name = 'The Chia Collection';
+    chiaCollection.date = 'Nov 2018';
+    chiaCollection.oneLiner = 'Clothing E-Commerce Platform';
+    chiaCollection.description =
+      'Designed and sold more than 150 articles of clothing worldwide.\n\n' +
+      "I've always enjoyed doodling as a child. Stepping out of my comfort zone, " +
+      'I took up this opportunity to start a business selling t-shirts and ' +
+      "hoodies with my own graphic designs.\n\n It's an honour to create something that " +
+      'resonates with someone and this journey has been a wild one.';
+    chiaCollection.techUsed = [
+      'Adobe Illustrator',
+      'Adobe Photoshop',
+      'cPanel',
+      'WooCommerce',
+      'WordPress',
+    ];
+    chiaCollection.image =
+      '../../assets/images/project-image-the-chia-collection.jpg';
     chiaCollection.action = "We're Closed";
-    chiaCollection.actionLink = "https://www.thechiacollection.com/";
-    chiaCollection.githubLink = "";
-
+    chiaCollection.actionLink = 'https://www.thechiacollection.com/';
+    chiaCollection.githubLink = '';
 
     const fintechSocietyNFT = new Project();
-    fintechSocietyNFT.name = "NFT Minting and Viewing Website";
-    fintechSocietyNFT.date = "Jan 2022";
-    fintechSocietyNFT.oneLiner = "NUS Fintech Society NFT Website",
-    fintechSocietyNFT.description = "I joined NUS Fintech Society in August and finally, " +
-                "I have the opportunity to build a blockchain project! " +
-                "\n\nTasked as the Project Lead, I led a group of 5 members to " +
-                "build NUS Fintech Society's first NFT Collection and Website. " +
-                "\n\nDelegated and integrated 3 main tasks well, designing the NFT " +
-                "and metadata, building the smart contract and developing the frontend.";
-    fintechSocietyNFT.techUsed = ["Material UI", "React.js", "Web3"];
-    fintechSocietyNFT.image = "../../assets/images/project-image-nft.jpg";
-    fintechSocietyNFT.action = "Demo Video";
-    fintechSocietyNFT.actionLink = "https://youtu.be/T_GE9wWe1v4";
-    fintechSocietyNFT.githubLink = "https://github.com/brendancjz/nus-fintech-society-nft";
+    fintechSocietyNFT.name = 'NFT Minting and Viewing Website';
+    fintechSocietyNFT.date = 'Jan 2022';
+    (fintechSocietyNFT.oneLiner = 'NUS Fintech Society NFT Website'),
+      (fintechSocietyNFT.description =
+        'I joined NUS Fintech Society in August and finally, ' +
+        'I have the opportunity to build a blockchain project! ' +
+        '\n\nTasked as the Project Lead, I led a group of 5 members to ' +
+        "build NUS Fintech Society's first NFT Collection and Website. " +
+        '\n\nDelegated and integrated 3 main tasks well, designing the NFT ' +
+        'and metadata, building the smart contract and developing the frontend.');
+    fintechSocietyNFT.techUsed = ['Material UI', 'React.js', 'Web3'];
+    fintechSocietyNFT.image = '../../assets/images/project-image-nft.jpg';
+    fintechSocietyNFT.action = 'Demo Video';
+    fintechSocietyNFT.actionLink = 'https://youtu.be/T_GE9wWe1v4';
+    fintechSocietyNFT.githubLink =
+      'https://github.com/brendancjz/nus-fintech-society-nft';
 
     const tompang = new Project();
-    tompang.name = "Tompang";
-    tompang.date = "Apr 2022";
-    tompang.oneLiner = "Mobile App: A Marketplace for Singaporean Travelers";
-    tompang.description = "Travelers can offer to buy back overseas products for interested " +
-          "buyers in Singapore, allowing travelers to earn extra income while they are on their " +
-          "trip. \n\nOn the flip side, Tompang offers Singaporeans the opportunity to either purchase " +
-          "products from overseas without bearing expensive shipping costs or purchase products that " +
-          "they would not have been able to purchase directly from Singapore.\n\n" +
-          "Tompang is an Ionic mobile application with an administrative JarkataEE web application, " +
-          "both leveraging on Glassfish. This project is completed within a semester under the module IS3106: Enterprise Systems Interface Design and Development.";
-    tompang.techUsed = ["CSS", "Glassfish", "HTML", "Ionic", "Jarkata EE", "Java", "Java Server Faces", "TypeScript"];
-    tompang.image = "../../assets/images/project-image-tompang.jpg";
-    tompang.action = "Demo Video";
-    tompang.actionLink = "https://youtu.be/r3e0ZtRd6tQ";
-    tompang.githubLink = "https://github.com/brendancjz/tompang";
+    tompang.name = 'Tompang';
+    tompang.date = 'Apr 2022';
+    tompang.oneLiner = 'Mobile App: A Marketplace for Singaporean Travelers';
+    tompang.description =
+      'Travelers can offer to buy back overseas products for interested ' +
+      'buyers in Singapore, allowing travelers to earn extra income while they are on their ' +
+      'trip. \n\nOn the flip side, Tompang offers Singaporeans the opportunity to either purchase ' +
+      'products from overseas without bearing expensive shipping costs or purchase products that ' +
+      'they would not have been able to purchase directly from Singapore.\n\n' +
+      'Tompang is an Ionic mobile application with an administrative JarkataEE web application, ' +
+      'both leveraging on Glassfish. \n\nThis project is completed within a semester under the module IS3106: Enterprise Systems Interface Design and Development.';
+    tompang.techUsed = [
+      'CSS',
+      'Glassfish',
+      'HTML',
+      'Ionic',
+      'Jarkata EE',
+      'Java',
+      'Java Server Faces',
+      'TypeScript',
+    ];
+    tompang.image = '../../assets/images/project-image-tompang.jpg';
+    tompang.action = 'Demo Video';
+    tompang.actionLink = 'https://youtu.be/r3e0ZtRd6tQ';
+    tompang.githubLink = 'https://github.com/brendancjz/tompang';
 
     const bestbudsbot = new Project();
-    bestbudsbot.name = "Best Buds Bot";
-    bestbudsbot.date = "June 2022";
-    bestbudsbot.oneLiner = "Birthday Tracking Bot for Telegram Groups";
-    bestbudsbot.description = "It is a hassle to discreetly collate birthday messages for a friend in a group. " +
-          "You will need to private message everyone else in the group. \n\n" +
-          "BestBudsBot helps to automate this process, ensuring everyone's birthday in the group is accounted for. " +
-          "Now, the birthday IC will not be left out too!";
-    bestbudsbot.techUsed = ["Heroku CLI", "Java", "Maven", "PostgreSQL", "Telegram API"];
-    bestbudsbot.image = "../../assets/images/project-image-bestbuds-bot.jpg";
-    bestbudsbot.action = "Try it out";
-    bestbudsbot.actionLink = "https://t.me/bbb_bestbuds_bot";
-    bestbudsbot.githubLink = "https://github.com/brendancjz/bestbuds-bot";
+    bestbudsbot.name = 'Best Buds Bot';
+    bestbudsbot.date = 'Jun 2022';
+    bestbudsbot.oneLiner = 'Birthday Tracking Bot for Telegram Groups';
+    bestbudsbot.description =
+      'It is a hassle to discreetly collate birthday messages for a friend in a group. ' +
+      'You will need to private message everyone else in the group. \n\n' +
+      "BestBudsBot helps to automate this process, ensuring everyone's birthday in the group is accounted for. " +
+      'Now, the birthday IC will not be left out too!';
+    bestbudsbot.techUsed = [
+      'Heroku CLI',
+      'Java',
+      'Maven',
+      'PostgreSQL',
+      'Telegram API',
+    ];
+    bestbudsbot.image = '../../assets/images/project-image-bestbuds-bot.jpg';
+    bestbudsbot.action = 'Try it out';
+    bestbudsbot.actionLink = 'https://t.me/bbb_bestbuds_bot';
+    bestbudsbot.githubLink = 'https://github.com/brendancjz/bestbuds-bot';
 
     const splitEase = new Project();
-    splitEase.name = "SplitEase";
-    splitEase.date = "April 2023";
-    splitEase.oneLiner = "SplitEase helps to split the gathering's bill with ease";
-    splitEase.description = "During my semester-long Student Exchange Program in the UK, my friends and I had trouble " +
-          "splitting the bill for our shared dinners. \n\nInterested in developing the most efficient " +
-          "algorithm to help split our bills equally. Within the week of 22 April 2023, SplitEase was born!";
-    splitEase.techUsed = ["Angular", "CSS", "HTML", "TypeScript"];
-    splitEase.action = "Try it out";
-    splitEase.actionLink = "https://splitease.brendanchia.com";
-    splitEase.image = "../../assets/images/project-image-splitease.jpg";
-    splitEase.githubLink = "https://github.com/brendancjz/splitease";
+    splitEase.name = 'SplitEase';
+    splitEase.date = 'Apr 2023';
+    splitEase.oneLiner =
+      "SplitEase helps to split the gathering's bill with ease";
+    splitEase.description =
+      'During my semester-long Student Exchange Program in the UK, my friends and I had trouble ' +
+      'splitting the bill for our shared dinners. \n\nInterested in developing the most efficient ' +
+      'algorithm to help split our bills equally. Within the week of 22 April 2023, SplitEase was born!';
+    splitEase.techUsed = ['Angular', 'CSS', 'HTML', 'TypeScript'];
+    splitEase.action = 'Try it out';
+    splitEase.actionLink = 'https://splitease.brendanchia.com';
+    splitEase.image = '../../assets/images/project-image-splitease.jpg';
+    splitEase.githubLink = 'https://github.com/brendancjz/splitease';
 
+    const virtualNursing = new Project();
+    virtualNursing.name = 'Virtual Nursing Dashboard';
+    virtualNursing.date = 'Nov 2023';
+    virtualNursing.oneLiner =
+      'Data-Driven Care, Anywhere: Bridging Nurses, Enhancing Lives';
+    virtualNursing.description =
+      "The hospital's smart wards, equipped with cutting-edge technology like smart beds and " +
+      'wearable patches, face a challenge in efficiently managing patient care due to a shortage of healthcare ' +
+      'professionals. Nurses grapple with vast amounts of clinical data from IoT sensors, necessitating a smart ward ' +
+      'analytics system. This system aims to consolidate and present patient data for efficient monitoring, ' +
+      'triggering alerts for nurses in cases of abnormalities or deterioration.\n\n' +
+      'Creating a prototype clinical dashboard and mobile app—Virtual Nursing Web Application & Virtual Nursing ' +
+      'Mobile Application (VNM)—to centralize patient data from smart beds and wearables. These tools enable virtual ' +
+      'nurses to monitor multiple patients remotely, customize data views, and alert ground nurses for timely patient ' +
+      'care, streamlining care processes and ensuring patient safety. \n\n' +
+      'This project is completed within a semester under the module IS4103: Information Systems Capstone Project.';
+    virtualNursing.techUsed = [
+      'AWS',
+      'Expo',
+      'Firebase',
+      'JavaScript',
+      'NodeJS',
+      'NextJS',
+      'React',
+      'React Native',
+      'Socket.io',
+      'TypeScript',
+    ];
+    virtualNursing.image = '../../assets/images/project-image-virtual-nursing-dashboard.jpg';
+    virtualNursing.action = 'Demo Video';
+    virtualNursing.actionLink = 'https://youtu.be/jNmDPle2pJY';
 
-    return [splitEase, bestbudsbot, tompang, fintechSocietyNFT, website, boLui, chiaCollection];
+    return [
+      virtualNursing,
+      splitEase,
+      bestbudsbot,
+      tompang,
+      fintechSocietyNFT,
+      website,
+      boLui,
+      chiaCollection,
+    ];
   }
 }
